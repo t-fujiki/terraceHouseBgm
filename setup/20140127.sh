c6d0fb90d2ead617580f22a0b9133423398f7593
t-fujiki <takecian@gmail.com>
2014-03-05 08:20:01 +0900

    added new videos

---
+++ setup/20140127.sh
@@ -0,0 +1,51 @@
+db.videos.insert(
+	[
+{
+	artist: "住岡梨奈",
+	title: "言葉にしたいんだ",
+	vid: "qGYSaxsmpFw",
+	date: ISODate("2014-01-27T09:00:00Z"),
+	category: "",
+}
+,
+{
+	artist: "THE HIGH-LOWS",
+	title: "青春",
+	vid: "dsCw0z97hiA",
+	date: ISODate("2014-01-27T09:00:00Z"),
+	category: "",
+}
+,
+{
+	artist: "BRUNO MARS",
+	title: "If I Knew",
+	vid: "9YZpng2PL34",
+	date: ISODate("2014-01-27T09:00:00Z"),
+	category: "",
+}
+,
+{
+	artist: "the Fratellis",
+	title: "SEVEN NIGHTS SEVEN DAYS",
+	vid: "I5QL37Sk8lU",
+	date: ISODate("2014-01-27T09:00:00Z"),
+	category: "",
+}
+,
+{
+	artist: "ARMIN VAN BUUREN",
+	title: "Beautiful Life",
+	vid: "VAo-t6bmGbo",
+	date: ISODate("2014-01-27T09:00:00Z"),
+	category: "",
+}
+,
+{
+	artist: "SCREAMING MALDINI",
+	title: "THE AWAKENING",
+	vid: "5ehzMbNYfSE",
+	date: ISODate("2014-01-27T09:00:00Z"),
+	category: "",
+}
+
+]);
---
+++ setup/20140203.sh
@@ -0,0 +1,66 @@
+db.videos.insert(
+	[
+{
+	artist: "MOTLEY CRUE",
+	title: "GIRLS,GIRLS,GIRLS",
+	vid: "d2XdmyBtCRQ",
+	date: ISODate("2014-02-03T09:00:00Z"),
+	category: "",
+}
+,
+{
+	artist: "Diane Birch",
+	title: "ALL THE LOVE YOU GOT",
+	vid: "oV5771GkekU",
+	date: ISODate("2014-02-03T09:00:00Z"),
+	category: "",
+}
+,
+{
+	artist: "PARAMORE",
+	title: "Still Into You",
+	vid: "OblL026SvD4",
+	date: ISODate("2014-02-03T09:00:00Z"),
+	category: "",
+}
+,
+{
+	artist: "SIMPLE PLAN",
+	title: "The Rest of Us",
+	vid: "iHFpVniBcJM",
+	date: ISODate("2014-02-03T09:00:00Z"),
+	category: "",
+}
+,
+{
+	artist: "Beastie Boys",
+	title: "Sure Shot",
+	vid: "JhqyZeUlE8U",
+	date: ISODate("2014-02-03T09:00:00Z"),
+	category: "",
+}
+,
+{
+	artist: "dulcet featuring KENSHU",
+	title: "Light To You",
+	vid: "CqIquOGIIAU",
+	date: ISODate("2014-02-03T09:00:00Z"),
+	category: "",
+}
+,
+{
+	artist: "CYNDI LAUPER",
+	title: "All Through The Night",
+	vid: "yW8qxUitG-Q",
+	date: ISODate("2014-02-03T09:00:00Z"),
+	category: "",
+}
+,
+{
+	artist: "KODALINE",
+	title: "All I Want",
+	vid: "mtf7hC17IBM",
+	date: ISODate("2014-02-03T09:00:00Z"),
+	category: "",
+}
+]);
