5135a24fe2071df983a3ad7d711f92d7c15210c0
t-fujiki <takecian@gmail.com>
2014-03-04 23:30:49 +0900

    remove secure info

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,11 +1,6 @@
 mongo
 use terrace
 
-mongo paulo.mongohq.com:10043/app17861801 -u t-fujiki -p
-
-db.videos.update({_id:ObjectId("52236915a445eaae398d06d5")},{$set: {vid: "sHFDBP9VDAg" }});
-
-
 ■Step from scratch
 
 yo express
