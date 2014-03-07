2f8fc33b76cf48ead9946a29f8378c137a153fec
t-fujiki <takecian@gmail.com>
2014-03-08 00:02:55 +0900

    update

--- public/js/main.js
+++ public/js/main.js
@@ -1,41 +1,32 @@
-// 2. This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
 
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
 var player;
+var isReady = false;
+
 function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-      height: '300',
-      width: '400',
-      videoId: 'oR6okRuOLc8',
-      events: {
-        'onReady': onPlayerReady,
-        'onStateChange': onPlayerStateChange
-    }
-});
+    isReady = true;
 }
 
-// 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
     event.target.playVideo();
 }
 
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
 var done = false;
+
 function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-      setTimeout(stopVideo, 6000);
-      done = true;
-  }
+    //   if (event.data == YT.PlayerState.PLAYING && !done) {
+    //     setTimeout(stopVideo, 6000);
+    //     done = true;
+    // }
+    if (state == 0) { // play completed
+        var scope = angular.element($('#main')).scope().next();
+    }
 }
+
 function stopVideo() {
     player.stopVideo();
 }
@@ -47,17 +38,17 @@ function playerState(state) {
     }
 }
 
-function onYouTubePlayerReady(playerId) {
-    var player = document.getElementById('player');
-    player.addEventListener('onStateChange', 'playerState');
-    player.playVideo();
-}
+// function onYouTubePlayerReady(playerId) {
+//     var player = document.getElementById('player');
+//     player.addEventListener('onStateChange', 'playerState');
+//     player.playVideo();
+// }
 
 function toLocaleString(date) {
     var month = "0" + (date.getMonth() + 1);
     var day = "0" + date.getDate();
     return [
-    date.getFullYear(), month.substr(month.length - 2), day.substr(day.length - 2)].join('-');
+        date.getFullYear(), month.substr(month.length - 2), day.substr(day.length - 2)].join('-');
 }
 
 var mainCtrl = function($scope, $http) {
@@ -93,13 +84,13 @@ var mainCtrl = function($scope, $http) {
     };
 
     function playNext() {
-        while(true){
+        while (true) {
             current++;
             if (current == $scope.videos.length) {
                 current = 0;
             }
 
-            if($scope.videos[current].date == $scope.date){
+            if ($scope.videos[current].date == $scope.date) {
                 play($scope.videos[current]);
                 break;
             }
@@ -107,13 +98,13 @@ var mainCtrl = function($scope, $http) {
     };
 
     function playPrev() {
-        while(true){
+        while (true) {
             current--;
             if (current < 0) {
                 current = $scope.videos.length - 1;
             }
 
-            if($scope.videos[current].date == $scope.date){
+            if ($scope.videos[current].date == $scope.date) {
                 play($scope.videos[current]);
                 break;
             }
@@ -143,167 +134,168 @@ var mainCtrl = function($scope, $http) {
     }
 
     function set(video) {
-            // var params = {
-            //     allowScriptAccess: "always"
-            // };
-            // var atts = {
-            //     id: "player"
-            // };
+        $scope.currentVideo = video;
+        $scope.currentVideo.status = "playing";
 
-            $scope.currentVideo = video;
-            $scope.currentVideo.status = "playing";
-            // swfobject.embedSWF("http://www.youtube.com/v/" + video.vid + "?enablejsapi=1&playerapiid=ytplayer", "player", "400", "300", "8", null, null, params, atts);
-            inc(video);
-        }
+        player = new YT.Player('player', {
+            height: '300',
+            width: '400',
+            videoId: video.vid,
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            }
+        });
+        inc(video);
+    }
 
-        function filter(date){
-            var $container = $('#container');
-            var options = {},
+    function filter(date) {
+        var $container = $('#container');
+        var options = {},
 
             key = "filter";
-            value = date;
-            if (value != "all") {
-                options[key] = '.' + value;
-            } else {
-                options[key] = "*";
+        value = date;
+        if (value != "all") {
+            options[key] = '.' + value;
+        } else {
+            options[key] = "*";
+        }
+        // parse 'false' as false boolean
+        setTimeout(function() {
+            $container.isotope(options);
+        }, 50)
+    }
+
+    $scope.setLayout = function() {
+        console.log("[IN]setLayout");
+        var $container = $('#container');
+
+        $container.isotope({
+            itemSelector: '.element',
+            getSortData: {
+                date: function($elem) {
+                    var date = $elem.attr('date');
+                    return date;
+                },
+                count: function($elem) {
+                    var count = parseInt($elem.attr('count'), 10);
+                    return count;
+                }
             }
-            // parse 'false' as false boolean
-            setTimeout(function(){
-                $container.isotope(options);
-            },50)
-        }
+        });
+        console.log("[OUT]setLayout");
+    };
 
-        $scope.setLayout = function() {
-            console.log("[IN]setLayout");
-            var $container = $('#container');
-
-            $container.isotope({
-                itemSelector: '.element',
-                getSortData: {
-                    date: function($elem) {
-                        var date = $elem.attr('date');
-                        return date;
-                    },
-                    count: function($elem) {
-                        var count = parseInt($elem.attr('count'), 10);
-                        return count;
-                    }
-                }
+    function showVideos(date) {
+        $http.get('/api/video/list', {
+            params: null
+        }).success(function(data, status, headers, config) {
+            $scope.videos = data;
+            var count = 0;
+            angular.forEach($scope.videos, function(value, key) {
+                var date = new Date(value.date);
+                value.date = toLocaleString(date);
+                value.index = count;
+                count++;
             });
-            console.log("[OUT]setLayout");
-        };
+            // console.log($scope.videos);
+            var nextIndex = 0;
+            if ($scope.isRandom == true) {
+                nextIndex = Math.floor(Math.random() * $scope.videos.length);
+            }
+            set($scope.videos[nextIndex]);
+            current = nextIndex;
 
-        function showVideos(date){
-            $http.get('/api/video/list', {
-                params: null
-            }).success(function(data, status, headers, config) {
-                $scope.videos = data;
-                var count = 0;
-                angular.forEach($scope.videos, function(value, key) {
-                    var date = new Date(value.date);
-                    value.date = toLocaleString(date);
-                    value.index = count;
-                    count++;
-                });
-                // console.log($scope.videos);
-                var nextIndex = 0;
-                if ($scope.isRandom == true) {
-                    nextIndex = Math.floor(Math.random() * $scope.videos.length);
-                }
-                set($scope.videos[nextIndex]);
-                current = nextIndex;
+            if ($scope.date != undefined) {
+                filter($scope.date);
+            }
+        }).
+        error(function(data, status, headers, config) {
+            console.log('error at get list:' + status);
+        });
 
-                if($scope.date != undefined){
-                    filter($scope.date);
-                }
-            }).
-            error(function(data, status, headers, config) {
-                console.log('error at get list:' + status);
+    }
+
+    function setup() {
+        console.log("[IN]setup");
+
+        $http.get('/api/video/date', {}).success(function(data, status, headers, config) {
+            $scope.dates = [];
+            // $scope.dates.push("all"); //
+            angular.forEach(data, function(value, key) {
+                // var date = new Date(value);
+                // $scope.dates.push(toLocaleString(date));
+                $scope.dates.push(value);
             });
+            $scope.date = $scope.dates[0];
+            showVideos($scope.date);
 
-        }
+        }).error(function(data, status, headers, config) {
+            console.log('error at get date:' + status);
+        });
 
-        function setup() {
-            console.log("[IN]setup");
+        console.log("[OUT]setup");
+    }
 
-            $http.get('/api/video/date', {}).success(function(data, status, headers, config) {
-                $scope.dates = [];
-                // $scope.dates.push("all"); //
-                angular.forEach(data, function(value, key) {
-                    // var date = new Date(value);
-                    // $scope.dates.push(toLocaleString(date));
-                    $scope.dates.push(value);
-                });
-                $scope.date = $scope.dates[0];
-                showVideos($scope.date);
+    setup();
 
-            }).error(function(data, status, headers, config) {
-                console.log('error at get date:' + status);
-            });
-
-            console.log("[OUT]setup");
-        }
-
+    $scope.showall = function($event) {
         setup();
+    }
 
-        $scope.showall = function($event) {
-            setup();
+    $scope.dateSelected = function($date) {
+        if ($scope.date != undefined) {
+            // showVideos($scope.date);
+            filter($scope.date)
         }
+    }
 
-        $scope.dateSelected = function($date) {
-            if($scope.date != undefined){
-                // showVideos($scope.date);
-                filter($scope.date)
-            }
-        }
+    $scope.sort = function($event) {
+        console.log($event.srcElement.value);
+        var $container = $('#container');
 
-        $scope.sort = function($event) {
-            console.log($event.srcElement.value);
-            var $container = $('#container');
+        // don't proceed if already selected
+        // if ($this.hasClass('selected')) {
+        //     return false;
+        // }
+        // make option object dynamically, i.e. { filter: '.my-filter-class' }
+        var options = {};
 
-            // don't proceed if already selected
-            // if ($this.hasClass('selected')) {
-            //     return false;
-            // }
-            // make option object dynamically, i.e. { filter: '.my-filter-class' }
-            var options = {};
+        key = "sortBy";
+        value = $event.srcElement.value;
+        options[key] = value;
 
-            key = "sortBy";
-            value = $event.srcElement.value;
-            options[key] = value;
-
-            if(previousSortState.target == value)
-            {
-                if(previousSortState.order == 0){
-                    options["sortAscending"] = true;
-                    previousSortState.order = 1;
-                    console.log(options);
-                }else{
-                    options["sortAscending"] = false;
-                    previousSortState.order = 0;
-                    console.log(options);
-                }
-            }else{
+        if (previousSortState.target == value) {
+            if (previousSortState.order == 0) {
+                options["sortAscending"] = true;
+                previousSortState.order = 1;
+                console.log(options);
+            } else {
                 options["sortAscending"] = false;
                 previousSortState.order = 0;
+                console.log(options);
             }
-            previousSortState.target = value
-            // parse 'false' as false boolean
-            $container.isotope(options);
-
+        } else {
+            options["sortAscending"] = false;
+            previousSortState.order = 0;
         }
+        previousSortState.target = value
+        // parse 'false' as false boolean
+        $container.isotope(options);
 
-        $scope.select = function($event) {
-            // console.log($event);
-            play($scope.videos[$event.target.attributes['index'].value]);
-            current = $event.target.attributes['index'].value;
-        }
+    }
 
-        $scope.forward = function($event) {
-            $scope.next();
-        }
+    $scope.select = function($event) {
+        // console.log($event);
+        play($scope.videos[$event.target.attributes['index'].value]);
+        current = $event.target.attributes['index'].value;
+    }
 
-        $scope.backward = function($event) {
-            $scope.prev();
-        }
-    };
+    $scope.forward = function($event) {
+        $scope.next();
+    }
+
+    $scope.backward = function($event) {
+        $scope.prev();
+    }
+};
