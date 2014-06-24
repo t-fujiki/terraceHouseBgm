47ab5335947cc00e2d3272220d3ad6c9f305d90b
t-fujiki <takecian@gmail.com>
2014-06-24 19:55:40 +0900

    update

---
+++ setup/2014/20140616.js
@@ -0,0 +1,45 @@
+db.videos.insert(
+[
+{
+	artist: "R3hab ＆ NERVO ＆ Ummet Ozcan",
+	title: "Revolution",
+	vid: "TWwIztjs29Y",
+	date: ISODate("2014-06-16T09:00:00Z"),
+	category: "",
+},
+{
+	artist: "OVERGROUND ACOUSTIC UNDERGROUND",
+	title: "夢の跡",
+	vid: "JZlBddtFHKI",
+	date: ISODate("2014-06-16T09:00:00Z"),
+	category: "",
+},
+{
+	artist: "KULA SHAKER",
+	title: "Hey Dude",
+	vid: "KDHd-vxcMDo",
+	date: ISODate("2014-06-16T09:00:00Z"),
+	category: "",
+},
+{
+	artist: "さかいゆう",
+	title: "薔薇とローズ",
+	vid: "yL3rDqAXR9E",
+	date: ISODate("2014-06-16T09:00:00Z"),
+	category: "",
+},
+{
+	artist: "Lily Allen",
+	title: "Somewhere Only We Know",
+	vid: "Ve9cBwI-pAg",
+	date: ISODate("2014-06-16T09:00:00Z"),
+	category: "",
+},
+{
+	artist: "Keane",
+	title: "Somewhere Only We Know",
+	vid: "Oextk-If8HQ",
+	date: ISODate("2014-06-16T09:00:00Z"),
+	category: "",
+}
+]);
---
+++ setup/2014/20140623.js
@@ -0,0 +1,45 @@
+db.videos.insert(
+[
+{
+	artist: "Jack's Mannequin",
+	title: "I'm Ready",
+	vid: "Qzvu_Qol9kA",
+	date: ISODate("2014-06-23T09:00:00Z"),
+	category: "",
+},
+{
+	artist: "The Isley Brothers",
+	title: "LOVE THE ONE YOU'RE WITH",
+	vid: "2Bo-PCf5Y0o",
+	date: ISODate("2014-06-23T09:00:00Z"),
+	category: "",
+},
+{
+	artist: "さかいゆう",
+	title: "薔薇とローズ",
+	vid: "yL3rDqAXR9E",
+	date: ISODate("2014-06-23T09:00:00Z"),
+	category: "",
+},
+{
+	artist: "KASABIAN",
+	title: "eez-eh",
+	vid: "ST6nEvIEY4s",
+	date: ISODate("2014-06-23T09:00:00Z"),
+	category: "",
+},
+{
+	artist: "NO USE FOR A NAME",
+	title: "Soulmate",
+	vid: "DaF2mxsTREU",
+	date: ISODate("2014-06-23T09:00:00Z"),
+	category: "",
+},
+{
+	artist: "BIRDY",
+	title: "Skinny Love",
+	vid: "aNzCDt2eidg",
+	date: ISODate("2014-06-23T09:00:00Z"),
+	category: "",
+}
+]);
